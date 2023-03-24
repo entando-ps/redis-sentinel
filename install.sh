d61342b1abe302b27bc167caec43796fcc6d4304
pietrangelo <p.masala@entando.com>
2023-03-24 07:14:29 +0100

    fix: fixed typo

--- install.sh
+++ install.sh
@@ -5,4 +5,4 @@ helm install redis bitnami/redis --values values.yaml
 
 sleep 60 # Wait 60 seconds to have the entire cluster deployed and than retrieve the master and slaves internal domain names
 
-kubeclt logs -f redis-node-0 -c sentinel
+kubectl logs -f redis-node-0 -c sentinel
